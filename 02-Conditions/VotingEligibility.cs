e696d87df5f910b03baa9b64968d003a11171578
Divya Pundhir <divyapundhir21@gmail.com>
2026-07-25 23:16:42 +0530

    Added some edge cases

--- 02-Conditions/VotingEligibility.cs
+++ 02-Conditions/VotingEligibility.cs
@@ -4,7 +4,7 @@ Check whether a person is eligible to vote.
 
 Algorithm:
 1. Read the person's age.
-2. If age is less than 0, print "Invalid age."
+2. If age is less than 0 or greater than 120, print "Invalid age."
 3. Else if age is greater than or equal to 18, print "Eligible to vote."
 4. Otherwise, print "Not eligible to vote."
 
@@ -20,18 +20,23 @@ O(1)
 Space Complexity:
 O(1)
 */
+
 using System;
 
 namespace CSharpPrograms
 {
     internal class VotingEligibility
     {
-        public static void Run()
+        static void Main(string[] args)
         {
             Console.Write("Enter your age: ");
             int age = Convert.ToInt32(Console.ReadLine());
 
-            if (age >= 18)
+            if (age < 0 || age > 120)
+            {
+                Console.WriteLine("Invalid age.");
+            }
+            else if (age >= 18)
             {
                 Console.WriteLine("Eligible to vote.");
             }
